af83fe238845f1312b2ccc59c17bab1159b5ff01
Alejandrox27 <didieralejandromejia82@gmail.com>
2026-07-29 20:03:27 -0500

    Separar la pestaña de postulaciones en una carpeta nueva para el apartado de convocatorias

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import psycopg2
 import pandas as pd
+from convocatorias.postulacion import mostrar_postulacion
 
 # 1. Configuración de título en la web
 st.title("🎓 Sistema de Movilidad - ORI UNAL")
@@ -33,16 +34,4 @@ with tab1:
     st.dataframe(df_estudiantes)
 
 with tab2:
-    st.header("Nueva Postulación")
-    # Formulario rápido para insertar datos
-    with st.form("form_postulacion"):
-        estudiante_id = st.text_input("Documento del Estudiante")
-        convocatoria_id = st.number_input("ID Convocatoria", min_value=1)
-        prioridad = st.selectbox("Prioridad de Opción", [1, 2, 3])
-        
-        # Botón de envío
-        btn_guardar = st.form_submit_button("Registrar Postulación")
-        
-        if btn_guardar:
-            # Aquí ejecutas el INSERT INTO postulaciones...
-            st.success("¡Postulación registrada con éxito!")
+    mostrar_postulacion(obtener_conexion)
---
+++ convocatorias/postulacion.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def mostrar_postulacion(obtener_conexion=None):
+    st.header("Nueva Postulación")
+    # Formulario rápido para insertar datos
+    with st.form("form_postulacion"):
+        estudiante_id = st.text_input("Documento del Estudiante")
+        convocatoria_id = st.number_input("ID Convocatoria", min_value=1)
+        prioridad = st.selectbox("Prioridad de Opción", [1, 2, 3])
+        
+        # Botón de envío
+        btn_guardar = st.form_submit_button("Registrar Postulación")
+        
+        if btn_guardar:
+            # Aquí ejecutas el INSERT INTO postulaciones...
+            st.success("¡Postulación registrada con éxito!")
